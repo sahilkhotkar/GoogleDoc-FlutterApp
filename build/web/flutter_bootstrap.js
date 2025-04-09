abf43d41b0c4a21c7a28892920a6c5a4cfe2f644
sahil_khotkar <khotkar.sahil@gmail.com>
2025-04-09 12:27:38 +0530

    update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2290646606"
+    serviceWorkerVersion: "1348439041"
   }
 });
